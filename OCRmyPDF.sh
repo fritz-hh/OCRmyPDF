b6a1db50fa067750ff7398cfdefa7849f96ac1d9
Max Hohenegger <github.com@hohenegger.eu>
2014-01-09 19:29:28 +0100

    fixes issue with empty FORCE_OCR parameter
    ./src/ocrPage.sh: 115: [: Illegal number:
    ./src/ocrPage.sh: 117: [: Illegal number:

--- OCRmyPDF.sh
+++ OCRmyPDF.sh
@@ -221,7 +221,7 @@ numpages=`tail -n 1 "$FILE_PAGES_INFO" | cut -f1 -d" "`
 # process each page of the input pdf file
 parallel -q -k --halt-on-error 1 "$OCR_PAGE" "$FILE_INPUT_PDF" "{}" "$numpages" "$TMP_FLD" \
 	"$VERBOSITY" "$LAN" "$KEEP_TMP" "$PREPROCESS_DESKEW" "$PREPROCESS_CLEAN" "$PREPROCESS_CLEANTOPDF" "$OVERSAMPLING_DPI" \
-	"$PDF_NOIMG" "$TESS_CFG_FILES" "$FORCE_OCR" < "$FILE_PAGES_INFO"
+	"$PDF_NOIMG" "\"$TESS_CFG_FILES\"" "$FORCE_OCR" < "$FILE_PAGES_INFO"
 ret_code="$?"
 [ $ret_code -ne 0 ] && exit $ret_code 
 
